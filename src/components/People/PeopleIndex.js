c0089d7abbe92095993d4197b93f2154d3de665a
Miguel Rafael González Durón <miguel.gonzalez@corp.base22.com>
2018-11-25 15:47:53 -0600

    Add Loader to PeopleIndex

--- src/components/People/PeopleIndex.js
+++ src/components/People/PeopleIndex.js
@@ -1,64 +1,58 @@
 import React from 'react';
-import * as peopleAPI from "../../api/PeopleRepository";
+import {PeopleRepository} from "../../api/PeopleRepository";
 import AddPersonContainer from "./AddPersonContainer";
-import {Card} from "./Card";
+import {Card} from "../Common/Card/Card";
+import {Loader} from "../Common/Loader/Loader";
+import {Utilities} from "../../utilities/Utilities";
 import classNames from "./PeopleStyles.module.css";
 
 class PeopleIndex extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            people: []
+            people: [],
+            fetch: false
         }
     }
 
     componentDidMount() {
-        peopleAPI.getPeople()
+        PeopleRepository.getPeople()
             .then(people => {
                 console.log(people);
-                this.setState({people})
+                this.setState({people, fetch: true})
             }).catch(e => {
             console.error(e);
         })
     }
 
     render() {
-        const {people = []} = this.state;
+        const {people = [], fetch} = this.state;
 
-        let peopleCard = [];
-        let row = [];
-        let counter = 0;
-        for (let i = 0; i < people.length; i++) {
-            row.push(people[i]);
-            counter++;
-            if (counter === 3) {
-                counter = 0;
-                peopleCard.push(row);
-                row = [];
-            }
-        }
+        let peopleCard = Utilities.distributeDataInBsRows(people, 3);
 
         return (
             <React.Fragment>
-                <div className="container-fluid">
-                    {
-                        peopleCard.map(row => (
-                            <div className={`row ${classNames["row--margin-top"]}`}>
-                                {
-                                    row.map(person => (
-                                        <div className="col-md-4">
-                                            <Card
-                                                title={person.name}
-                                                text={person.gender}
-                                                anchor={{link: person.id, label: "Detalle"}}
-                                            />
-                                        </div>
-                                    ))
-                                }
-                            </div>
-                        ))
-                    }
-                </div>
+                {
+                    !fetch ? <Loader/> :
+                        <div className="container-fluid">
+                            {peopleCard.map((row, key) => (
+                                <div key={key} className={`row ${classNames["row--margin-top"]}`}>
+                                    {
+                                        row.map((person, key) => (
+                                            person &&
+                                            <div key={key} className="col-md-4">
+                                                <Card
+                                                    title={person.name}
+                                                    text={person.gender}
+                                                    anchor={{link: person.id, label: "Detalle"}}
+                                                />
+                                            </div>
+                                        ))
+                                    }
+                                </div>
+                            ))}
+                        </div>
+                }
                 <AddPersonContainer people={this.state.people}/>
             </React.Fragment>
         );
